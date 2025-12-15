194841ca63b631511b3e2977da4f03984d0b81e1
Razwea77 <razweaisuen01@gmx.net>
2025-12-15 13:53:09 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,25 +8,11 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Menyay",
-  description: "Menyay App",
+  title: "SS.LV",
+  description: "Sludinājumi - SS.LV",
   generator: "v0.app",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+    icon: /favicon.png
   },
 }
 
